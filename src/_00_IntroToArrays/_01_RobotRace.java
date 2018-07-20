3ee18eaae8b2639d5369f1db1121ec1e08490c75
Elijah2005 <league@wts22.attlocal.net>
2018-07-19 18:27:14 -0700

    step 14 of find the hidden button

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -1,24 +1,47 @@
 package _00_IntroToArrays;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
+
 	// 1. make a main method
 	public static void main(String[] args) {
 
 		// 2. create an array of 5 robots.
-		Robot[] bot = new Robot[5];
+		Robot[] bot = new Robot[6];
 		// 3. use a for loop to initialize the robots.
 		for (int i = 0; i < bot.length; i++) {
 			bot[i] = new Robot();
-
-			// 4. make each robot start at the bottom of the screen, side by side, facing up
-
-			bot[0].moveTo(i * 115, i * 130);
 		}
+		// 4. make each robot start at the bottom of the screen, side by side, facing up
+		bot[0].moveTo(300, 550);
+		bot[1].moveTo(150, 550);
+		bot[2].moveTo(450, 550);
+		bot[3].moveTo(600, 550);
+		bot[4].moveTo(750, 550);
 		// 5. use another for loop to iterate through the array and make each robot move
 		// a random amount less than 50.
+		boolean raceIsOver = false;
+		while (!raceIsOver) {
+			for (int i = 0; i < bot.length; i++) {
+				Random rand = new Random();
+				bot[i].setSpeed(10);
+				bot[i].move(rand.nextInt(49));
+				bot[i].setRandomPenColor();
+				bot[i].penDown();
+				bot[i].getY();
+				if (bot[i].getY() <= 0) {
+					raceIsOver = true;
+					JOptionPane.showMessageDialog(null, "Robot # " + i + " is the winner");
+					bot[i].sparkle();
 
+				}
+			}
+		}
 		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
 		// screen.
 
