1f29bf9912f1ef27251caef471f090896bc21d86
League <league@WTS9.attlocal.net>
2018-08-16 18:27:29 -0700

    started firework step 3

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -24,16 +24,30 @@ public class _00_1D_Array_Methods {
 	// 3. Complete the method so that it returns true if the integer
 	// array contains the value specified by the second parameter.
 	// It should otherwise return false.
-	public static boolean containsIntValue(int[] array, int value) {
-
+	public static  Boolean containsIntValue(int[] array, int value) {
+	for (int i = 0; i < array.length; i++) {
+		if(array[i] == value) {
+			return true;
+		}
+	}
 		return false;
+	
+		
 	}
 
 	// 4. Complete the method so that it returns the index of the,
 	// first instance that the specified value occurs in the array.
 	// If the array does not contain the specified value, it should return -1.
 	public static int getIndex(int[] arr, int value) {
-
-		return 0;
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] == value) {
+				return i;
+			}
+		}
+		return -1;	
+		
+		
+		
+		}
 	}
-}
+
