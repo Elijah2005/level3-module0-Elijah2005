43bb94603ab129faef98de4f6585679e08c08013
Cainnan Carra <league@WTS22.attlocal.net>
2018-09-06 18:27:00 -0700

    finish step 5

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -1,32 +1,49 @@
 package _03_More_Array_Fun;
 
+import java.util.Random;
+
 public class MoreArrayFun {
-	// 1. Create a main method to test the other methods you write.
+
 	public static void main(String[] args) {
+	}
 
-		// 2. Write a method that takes an array of Strings and prints all the Strings
-		// in the array.
+	// 1. Create a main method to test the other methods you write.
+	// 2. Write a method that takes an array of Strings and prints all the Strings
+	// in the array.
+	void StringPrinter() {
 		String[] letters = { "a", "b", "c", "d", "e" };
-		// for (int i = 0; i < letters.length; i++) {
-		// System.out.println(letters[i]);
-		// }
+		for (int i = 0; i < letters.length; i++) {
+			System.out.println(letters[i]);
+		}
 
-		// 3. Write a method that takes an array of Strings and prints all the Strings
-		// in the array
-		// in reverse order.
-		// for (int j = letters.length - 1; j >= 0; j--) {
-		// System.out.println(letters[j]);
-		// }
+	}
+
+	// 3. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in reverse order.
+	void ReverseStringPrinter() {
+		String[] letters = { "f", "g", "h", "i", "j" };
+		for (int j = letters.length - 1; j >= 0; j--) {
+			System.out.println(letters[j]);
+
+		}
 
 		// 4. Write a method that takes an array of Strings and prints every other
 		// String in the array.
+
 		for (int i = 0; i < letters.length; i += 2) {
 			System.out.println(letters[i]);
 		}
+
 		// 5. Write a method that takes an array of Strings and prints all the Strings
 		// in the array
 		// in a completely random order. Almost every run of the program should result
 		// in a different order.
+		for (int i = 0; i < letters.length; i++) {
+			Random rand = new Random();
+
+		}
 
 	}
+
 }
