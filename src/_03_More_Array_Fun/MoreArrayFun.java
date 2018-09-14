872f12270bdb56d0ad6e9db7e2cecf47c651cd23
aleksaradomirovic <league@WTS22.attlocal.net>
2018-09-13 18:26:10 -0700

    fix arrays

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -5,6 +5,9 @@ import java.util.Random;
 public class MoreArrayFun {
 
 	public static void main(String[] args) {
+		MoreArrayFun array = new MoreArrayFun();
+		String[] letters = { "a", "b", "c", "d", "e" };
+		array.RandomString(letters);
 	}
 
 	// 1. Create a main method to test the other methods you write.
@@ -27,23 +30,40 @@ public class MoreArrayFun {
 			System.out.println(letters[j]);
 
 		}
+	}
 
-		// 4. Write a method that takes an array of Strings and prints every other
-		// String in the array.
-
+	// 4. Write a method that takes an array of Strings and prints every other
+	// String in the array.
+	void EveryOtherString() {
+		String[] letters = { "k", "l", "m", "n", "o" };
 		for (int i = 0; i < letters.length; i += 2) {
 			System.out.println(letters[i]);
 		}
+	}
 
-		// 5. Write a method that takes an array of Strings and prints all the Strings
-		// in the array
-		// in a completely random order. Almost every run of the program should result
-		// in a different order.
-		for (int i = 0; i < letters.length; i++) {
+	// 5. Write a method that takes an array of Strings and prints all the Strings
+	// in the array
+	// in a completely random order. Almost every run of the program should result
+	// in a different order.
+	void RandomString(String[] array) {
+		String[] letters = new String[array.length];
+		int numberPrinted = 0;
+		while (numberPrinted < array.length) {
+			Boolean words = true;
 			Random rand = new Random();
-
+			int random = rand.nextInt(array.length);
+			String randomWord = array[random];
+			for (int i = 0; i < letters.length; i++) {
+				if (randomWord == letters[i]) {
+					words = false;
+				}
+			}
+			if (words == true) {
+				System.out.println(randomWord);
+				letters[numberPrinted] = randomWord;
+				numberPrinted++;
+			}
 		}
 
 	}
-
 }
